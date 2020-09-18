495909e90c0840777a71a449cfb0c414db708e23
kingz10102 <kingz10102@gmail.com>
2020-09-17 23:41:37 -0400

    this is a commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import '../css/SideBar.css';
+import DonutLargeRoundedIcon from '@material-ui/icons/DonutLargeRounded';
+import { Avatar, IconButton } from '@material-ui/core';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
+import MoreVertRoundedIcon from '@material-ui/icons/MoreVertRounded';
+import { SearchOutlined } from "@material-ui/icons";
 function SideBar() {
+    // const [{ user }, dispatch] = useStateValue();
+
     return (
         <div className='sidebar'>
-            <h1>yoo</h1>
+            <div className='sidebar__heading'>
+                {/* <Avatar src={user?.photoURL}/> */}
+                <div className='sidebar__headingRight'>
+                    {/* makes it clickable */}
+                    <IconButton>
+                        <DonutLargeRoundedIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ForumRoundedIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertRoundedIcon/>
+                    </IconButton>
+                </div>
+            </div>
+
+             {/* search icon built-search engine */}
+             <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                <SearchOutlined/>
+              <input 
+              placeholder="Look up or chat away!" 
+              type="text"
+              />
+                </div>
+            </div>
+
         </div>
     )
 }
