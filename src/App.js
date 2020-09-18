c97de39dbce2dd9c5f89d7322a590648ef67adc7
kingz10102 <kingz10102@gmail.com>
2020-09-17 22:18:53 -0400

    this is a commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+	
+  
+  return(
+    <div className="app">
+      <div>
 
+      </div>
+    </div>
+);
+
+}
 export default App;
